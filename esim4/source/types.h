5be04860ca9becde9e8e92132bbe522316dbc53e
Peter Rice <peter.rice@i2b2transmart.org>
2022-06-24 12:21:23 +0100

    ESIM4 updated for EMBOSS 2.9.0

--- esim4/source/types.h
+++ esim4/source/types.h
@@ -1,11 +1,15 @@
 #ifndef SIM_TYPES_H
 #define SIM_TYPES_H
-/* $Id: types.h,v 1.1 2002/02/25 09:33:50 hgmp Exp $ */
+/* $Id: types.h,v 1.3 2004/06/16 10:02:28 rice Exp $ */
 
 #define NACHARS 128
 
 typedef int bool;
 typedef int ss_t[NACHARS][NACHARS];
+
+/* uchar is already defined in most platforms?  - but not linux at EBI */
+/*  how do we test? */
+
 typedef unsigned char uchar;
 
 #endif
