f5ebc2b34f74ab08255c7a4f745ce21ea71b5a85
Peter Rice <peter.rice@i2b2transmart.org>
2022-06-24 16:13:42 +0100

    DOMAINATRIX updated for EMBOSS 6.1.0

--- domainatrix/src/domainseqs.c
+++ domainatrix/src/domainseqs.c
@@ -127,7 +127,7 @@ int main(int argc, char **argv)
     
     ajint      type       = 0;   /* Type of domain (ajSCOP or ajCATH) in the 
 				    DCF file.                                */
-
+    AjPPdbtosp pdbtosp = NULL;
     
     /* Initialise strings, lists,  etc */
     msg        = ajStrNew();
@@ -264,7 +264,7 @@ int main(int argc, char **argv)
 	ajStrAssignC(&db, "swissprot-acc:");
 	ajStrAppendS(&db, acc);
 
-	if(!(ajSeqGetFromUsa(db,ajTrue,&sp_seq)))
+	if(!(ajSeqGetFromUsa(db,ajTrue,sp_seq)))
         {
             ajFmtPrintF(errf, "%-15s\n", "ENTRY_NOT_FOUND_IN_SW");
             ajDomainWrite(domain_outf,domain);
@@ -356,6 +356,11 @@ int main(int argc, char **argv)
 
     
     /* Memory management.  */
+
+    while(ajListPop(list, (void**) &pdbtosp))
+        ajPdbtospDel(&pdbtosp);
+    ajListFree(&list);
+
     ajDirDel(&dpdb_path);
     ajMatrixfDel(&matrix);
     ajStrDel(&msg);
